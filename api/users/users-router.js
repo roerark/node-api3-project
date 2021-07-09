aea394d57f7cafe2bf68b01cebb10bc101a63ded
joe-park <parkjs3@yahoo.com>
2021-07-09 13:02:13 -0700

    mvp IP

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,30 +3,40 @@ const { validateUserId, validateUser } = require("../middleware/middleware");
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
+const User = require("./users-model");
+const Post = require("../posts/posts-model");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get("/", (req, res, next) => {
+  User.get()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch(next);
 });
 
 router.get("/:id", validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user);
+  req.json(req.user);
 });
 
-router.post("/", validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
-  console.log(req.user);
+router.post("/", validateUser, (req, res, next) => {
+  User.insert({ name: req.name })
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
 });
 
-router.put("/:id", validateUserId, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user);
-  console.log(req.text);
+router.put("/:id", validateUserId, (req, res, next) => {
+  User.update(req.params.id, { name: req.name })
+    .then(() => {
+      return User.getById(req.params.id);
+    })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch(next);
 });
 
 router.delete("/:id", validateUserId, (req, res) => {
@@ -46,6 +56,15 @@ router.post("/:id/posts", validateUserId, validateUser, (req, res) => {
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
   console.log(req.user);
+  console.log(req.text);
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: "something tragic inside posts router happened",
+    err: err.message,
+    stack: err.stack,
+  });
 });
 
 // do not forget to export the router
